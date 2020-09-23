1d8c635847ff3cc71a830b308fc6cb0c41a35a81
jihun92 <jihunkim921114@gmail.com>
2020-09-23 08:10:18 +0000

    update install for ansible, pip

---
+++ install/ansible/install_ansible.sh
@@ -0,0 +1,10 @@
+pip install --user --no-deps ./dependency_whl/cffi-1.14.3-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/cryptography-3.1-cp27-cp27mu-manylinux2010_x86_64.whl
+pip install --user --no-deps ./dependency_whl/enum34-1.1.10-py2-none-any.whl
+pip install --user --no-deps ./dependency_whl/ipaddress-1.0.23-py2.py3-none-any.whl
+pip install --user --no-deps ./dependency_whl/MarkupSafe-1.1.1-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/Jinja2-2.11.2-py2.py3-none-any.whl
+pip install --user --no-deps ./dependency_whl/pycparser-2.20-py2.py3-none-any.whl
+pip install --user --no-deps ./dependency_whl/PyYAML-5.3.1.tar.gz
+pip install --user --no-deps ./dependency_whl/six-1.15.0-py2.py3-none-any.whl
+pip install --user --no-deps ansible-2.9.11.tar.gz
---
+++ install/python/pip/install_pip.sh
@@ -0,0 +1,1 @@
+python pip-20.2.3-py2.py3-none-any.whl/pip install --user pip-20.2.3-py2.py3-none-any.whl
