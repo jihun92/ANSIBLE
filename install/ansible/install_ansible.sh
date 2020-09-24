92b685185f33117affe2f29cb98a2cba8c9175ef
jihun92 <48423517+jihun92@users.noreply.github.com>
2020-09-25 00:28:41 +0900

    Update install_ansible.sh

--- install/ansible/install_ansible.sh
+++ install/ansible/install_ansible.sh
@@ -1,10 +1,18 @@
-pip install --user --no-deps ./dependency_whl/cffi-1.14.3-cp27-cp27m-manylinux1_x86_64.whl
-pip install --user --no-deps ./dependency_whl/cryptography-3.1.1-cp27-cp27m-manylinux1_x86_64.whl
 pip install --user --no-deps ./dependency_whl/enum34-1.1.10-py2-none-any.whl
 pip install --user --no-deps ./dependency_whl/ipaddress-1.0.23-py2.py3-none-any.whl
-pip install --user --no-deps ./dependency_whl/MarkupSafe-1.1.1-cp27-cp27m-manylinux1_x86_64.whl
 pip install --user --no-deps ./dependency_whl/Jinja2-2.11.2-py2.py3-none-any.whl
 pip install --user --no-deps ./dependency_whl/pycparser-2.20-py2.py3-none-any.whl
 pip install --user --no-deps ./dependency_whl/PyYAML-5.3.1.tar.gz
 pip install --user --no-deps ./dependency_whl/six-1.15.0-py2.py3-none-any.whl
+
+pip install --user --no-deps ./dependency_whl/mu/MarkupSafe-1.1.1-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/mu/PyNaCl-1.4.0-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/mu/bcrypt-3.1.7-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/mu/cffi-1.14.3-cp27-cp27mu-manylinux1_x86_64.whl
+pip install --user --no-deps ./dependency_whl/mu/cryptography-3.1-cp27-cp27mu-manylinux2010_x86_64.whl
+
+# pip install
+pip install --user --no-deps ./dependency_whl/paramiko-2.7.2-py2.py3-none-any.whl
+
+# ansible install
 pip install --user ansible-2.9.11.tar.gz
